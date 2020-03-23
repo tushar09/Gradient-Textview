ab43e5a2d63071007ac7279495e449eccc1c506e
Tushar <wtushar09@gmail.com>
2020-03-24 03:16:51 +0600

    doc added

--- tvg/src/main/java/com/captaindroid/tvg/Tvg.java
+++ tvg/src/main/java/com/captaindroid/tvg/Tvg.java
@@ -6,12 +6,26 @@ import android.graphics.Shader;
 import android.widget.TextView;
 
 public class Tvg {
+    /**
+     * Changes the textview's color to gradient
+     *
+     * @param tv TextView to change the color
+     * @param startColor first color of gradient
+     * @param endColor end color of gradient
+     */
     public static void change(TextView tv, int startColor, int endColor){
         Shader textShader = new LinearGradient(0, 0, tv.getPaint().measureText(tv.getText().toString()), tv.getTextSize(),
                 new int[]{startColor, endColor},
                 new float[]{0, 1}, Shader.TileMode.CLAMP);
         tv.getPaint().setShader(textShader);
     }
+
+    /**
+     * Changes the textview's color to gradient
+     *
+     * @param tv TextView to change the color
+     * @param colors arrays of multiple colors to create the gradient
+     */
     public static void change(TextView tv, int [] colors){
         Shader textShader = new LinearGradient(0, 0, tv.getPaint().measureText(tv.getText().toString()), tv.getTextSize(),
                 colors,
