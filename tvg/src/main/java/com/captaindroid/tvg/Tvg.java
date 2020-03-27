7a551eb95aa6eddd2ee789e1d0112994d03a0bfe
Tushar <wtushar09@gmail.com>
2020-03-27 23:27:00 +0600

    2 method added which can change the list of textviews

--- tvg/src/main/java/com/captaindroid/tvg/Tvg.java
+++ tvg/src/main/java/com/captaindroid/tvg/Tvg.java
@@ -1,10 +1,11 @@
 package com.captaindroid.tvg;
 
-import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Shader;
 import android.util.Log;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class Tvg {
     /**
@@ -38,7 +39,48 @@ public class Tvg {
         }catch (Exception e){
             Log.e(Tvg.class.getSimpleName(), e.toString());
         }
+    }
 
+    /**
+     * Changes the list of textview's color to gradient
+     *
+     * @param tvs List of Textviews which are need to be changed
+     * @param colors arrays of multiple colors to create the gradient
+     */
+    public static void change(List<TextView> tvs, int [] colors){
+        try {
+            for (TextView tv : tvs) {
+                tv.setTextColor(colors[0]);
+                Shader textShader = new LinearGradient(0, 0, tv.getPaint().measureText(tv.getText().toString()), tv.getTextSize(),
+                        colors,
+                        null, Shader.TileMode.CLAMP);
+                tv.getPaint().setShader(textShader);
+            }
+
+        }catch (Exception e){
+            Log.e(Tvg.class.getSimpleName(), e.toString());
+        }
+    }
+
+    /**
+     * Changes the list of textview's color to gradient
+     *
+     * @param tvs List of Textviews which are need to be changed
+     * @param startColor first color of gradient
+     * @param endColor end color of gradient
+     */
+    public static void change(List<TextView> tvs, int startColor, int endColor){
+        try {
+            for (TextView tv : tvs) {
+                tv.setTextColor(startColor);
+                Shader textShader = new LinearGradient(0, 0, tv.getPaint().measureText(tv.getText().toString()), tv.getTextSize(),
+                        new int[]{startColor, endColor},
+                        new float[]{0, 1}, Shader.TileMode.CLAMP);
+                tv.getPaint().setShader(textShader);
+            }
+        }catch (Exception e){
+            Log.e(Tvg.class.getSimpleName(), e.toString());
+        }
 
     }
 }
